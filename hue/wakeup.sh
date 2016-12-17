f87952633ac7ebbbd882fd0b0221c7afd578832f
missinglink <insomnia@rcpt.at>
2016-12-17 14:21:03 +0000

    hue: wakeup light

--- hue/wakeup.sh
+++ hue/wakeup.sh
@@ -22,13 +22,10 @@ function off(){ send '{ "on": false }'; }
 # brightness (0-254) - saturation/intensity (0-254) - hue/colour (0-65535)
 function update(){ send "{ \"on\": true, \"bri\": $1, \"sat\": $2, \"hue\": $3 }"; }
 
-# update 0 0 0;
-off; exit;
-
 # sunrise settings
 STEPS='127';
 DURATION_MINS='30';
-DURATION_SECS=$(echo "scale = 2; $DURATION_MINS * 60 / $STEPS" | bc);
+SLEEP_SECS=$(echo "scale = 2; $DURATION_MINS * 60 / $STEPS" | bc);
 HUE_MULTIPLIER='25';
 HUE_OFFSET='+6375';
 
@@ -37,5 +34,9 @@ for ((i=0;i<=$STEPS;i++)); do
   SAT=$(echo "scale = 0; 254 - $i" | bc);
   HUE=$(echo "scale = 0; $HUE_MULTIPLIER * $i $HUE_OFFSET" | bc);
   update $i $SAT $HUE;
-  sleep $DURATION_SECS;
+  sleep $SLEEP_SECS;
 done;
+
+# turn light(s) off
+sleep 2700; # 45 mins
+off;
